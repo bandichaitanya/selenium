4efccaa0249b79744e1e0bc188e39075032e3cc7
bandichaitanya <168046018+bandichaitanya@users.noreply.github.com>
2025-07-18 14:06:45 +0530

    Update LaunchFb.java

--- src/test/java/TestNG_Implimentation/LaunchFb.java
+++ src/test/java/TestNG_Implimentation/LaunchFb.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 
 
 public class LaunchFb {
+	//This is Lunch Facebook progm
 	@Parameters("browser")
 	@Test
 	public void fbTest(String BROWSER) throws InterruptedException
